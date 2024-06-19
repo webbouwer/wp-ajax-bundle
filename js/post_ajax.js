fb1e399fad0a017743c97ba5f3c15349d3010c63
webbouwer <support@webdesigndenhaag.net>
2024-06-19 22:32:01 +0200

    Popup Eventinfo (centered)

--- js/post_ajax.js
+++ js/post_ajax.js
@@ -49,7 +49,6 @@ jQuery(function ($) {
       };
 
       if (data.posttype != '') {
-
         // reqvars.posttype = data.posttype; 
         // replaced with object request for multiple posttype 
         let posttypevars = {};
@@ -121,17 +120,12 @@ jQuery(function ($) {
 
   }
 
-
-
+  // prepare data pull
   function getPostData(args = false) {
-
-
     let reqdata = data_args_default; // set default variables
-
     if (pullflag) { // if no requests active
       pullflag = false;
       pullpage++;
-
       reqdata['page'] = pullpage; // set query pagenumber
       if (args) { // args from the trigger function (load/button/scroll)
         for (const key in data_args_default) {
@@ -140,12 +134,10 @@ jQuery(function ($) {
           }
         }
       }
-      getPosts(reqdata);
-      //console.log( reqdata );
+      getPosts(reqdata);  //console.log( reqdata );
     }
-
   }
-
+  // pull data
   function getPosts(args) {
 
     jQuery.ajax({
@@ -158,10 +150,7 @@ jQuery(function ($) {
         data: args
       },
       success: function (response) {
-
-        console.log( JSON.stringify(args) );
-
-
+        //console.log( JSON.stringify(args) );
         setPostsHTML(response); // JSON.stringify(response)
         if ($('.wpajaxbundle.loader').length > 0) {
           $('.wpajaxbundle.loader').fadeOut();
@@ -174,98 +163,98 @@ jQuery(function ($) {
         }
       },
       error: function (XMLHttpRequest, textStatus, errorThrown) {
-        //Error
-        console.log( textStatus ); 
+        console.log(textStatus); //Error
       },
       timeout: 6000
     });
-    return false; 
-
+    return false;
   }
 
+  // response to html
   function setPostsHTML(result) {
 
-    //console.log( result );
-    //let t = 0; // timer for smooth slowed-down slide-in
-    
-     // setup  eventlist 
-    var calEvents = []; // if event type create list for calendar view
+    // eventlist holder
+    var calEvents = []; // if event-type create list for calendar view
 
-    $.each(result, function (idx, post) {
-
-      //console.log(JSON.stringify(post));
+    $.each(result, function (idx, post) { //console.log(JSON.stringify(post));
 
       // add event to eventlist 
       if (post.type == 'event' && post.custom_field_values['event-start-date'].length > 0) {
-
-        let start_gmt = stampToGMTdateString(post.custom_field_values['event-start-date']);
-        var event = { 
-          id: post.id, 
+        //https://fullcalendar.io/docs/event-object
+        //https://fullcalendar.io/docs/eventDisplay
+        // build event object 
+        var event = {
+          id: post.id,
           title: post.title,
-					start: start_gmt,//'2024-06-19T10:30:00', //end: end_gmt, //'2024-06-20T12:30:00' 
+          'event-summary': post.excerpt,
+          content: post.content,
+          imgurl: post.imgurl,
+          showendtime: post.custom_field_values["event-hide-end-time"],
+          allDay: post.custom_field_values["event-all-day"],
+          location: post.custom_field_values["event-location"],
+          link: post.custom_field_values["event-link"],
+          linktext: post.custom_field_values["event-link-label"],
+          linktarget: post.custom_field_values["event-link-target"],
+          linktitle: post.custom_field_values["event-link-title"],
+          linkimg: post.custom_field_values["event-link-image"],
+          imgthumbid: post.custom_field_values["_thumbnail_id"],
+          //backgroundColor: 'green',
         };
-        // retrieve variables
-        if(post.custom_field_values['event-date'] != ''){
-          let end_gmt = stampToGMTdateString(post.custom_field_values['event-date']);
-          event['end'] = end_gmt; 
+        // add filtered/retrieved variables
+        event['start']  = stampToGMTdateString(post.custom_field_values['event-start-date']);
+        //'2024-06-19T10:30:00', //end: end_gmt, //'2024-06-20T12:30:00' 
+        if (post.custom_field_values['event-date'] != '') {
+          event['end']  = stampToGMTdateString(post.custom_field_values['event-date']);
         }
-          
+        event['url'] = post.link;
         calEvents[idx] = event;
 
-      }else{ // regular post list
-      let objfilterclasses = 'item';
-      let obj = $('<div id="post-' + post.id + '"></div>');
+      } else { // post list
 
-      obj.attr('data-tags', post.tags.toString());
-      obj.attr('data-cats', post.cats.toString());
+        let t = 0; // timer for smooth slowed-down slide-in
+        let objfilterclasses = 'item';
+        let obj = $('<div id="post-' + post.id + '"></div>');
+        obj.attr('data-tags', post.tags.toString());
+        obj.attr('data-cats', post.cats.toString());
 
-      $(post.tags).each(function (x, tag) {
-        objfilterclasses += ' ' + tag;
-      });
+        $(post.tags).each(function (x, tag) {
+          objfilterclasses += ' ' + tag;
+        });
+        obj.attr('class', objfilterclasses);
 
-      obj.attr('class', objfilterclasses);
+        let title = $('<h2><a href="' + post.link + '">' + post.title + '</a></h2>');
+        obj.append(title);
+        let content = $('<div class="content">' + post.excerpt + '</div>');
+        if (post.display != 'excerpt') {
+          content = $('<div class="excerpt">' + post.content + '</div>');
+        }
+        obj.append(content);
+        let cats = $('<div class="cats" />');
+        for (c = 0; c < post.cats.length; c++) {
+          cats.append('<span>' + post.cats[c]) + '</span>';
+        }
+        obj.append(cats);
+        let tags = $('<div class="tags" />');
+        for (s = 0; s < post.tags.length; s++) {
+          tags.append('<span>' + post.tags[s]) + '</span>';
+        }
+        obj.append(tags);
+        obj.hide(); // slowed-down slide-in
+        $('body').find('.wpajaxbundle .itemcontainer').append(obj);
 
-      let title = $('<h2><a href="' + post.link + '">' + post.title + '</a></h2>');
-      obj.append(title);
-
-      let content = $('<div class="content">' + post.excerpt + '</div>');
-      if (post.display != 'excerpt') {
-        content = $('<div class="excerpt">' + post.content + '</div>');
-      }
-      obj.append(content);
-
-
-      let cats = $('<div class="cats" />');
-      for (c = 0; c < post.cats.length; c++) {
-        cats.append('<span>' + post.cats[c]) + '</span>';
-      }
-      obj.append(cats);
-
-      let tags = $('<div class="tags" />');
-      for (s = 0; s < post.tags.length; s++) {
-        tags.append('<span>' + post.tags[s]) + '</span>';
-      }
-      obj.append(tags);
-
-      $('body').find('.wpajaxbundle .itemcontainer').append(obj);
-
-      /*let obj = $('<div id="post-'+post.id+'">'+post.title+'</div>').hide();//.slideUp(300);
-      $('body').find('.wpajaxbundle.button').parent().find('.itemcontainer').append(obj);
-      // slowed-down slide-in
-      setTimeout(function(){
-        obj.slideDown(300);
-      },t);
-      t=(t+50);*/
-
-      if ($('#wpajaxbundle').data('load') == 'all') {
-        /* repeat ppp load automaticaly untill all is loaded  */
+        // slowed-down slide-in
         setTimeout(function () {
-          doRequestData();
-        }, 100);
-      }
+          obj.slideDown(300);
+        }, t);
+        t = (t + 50);
 
-    } // end regular post
-
+        if ($('#wpajaxbundle').data('load') == 'all') {
+          /* repeat ppp load automaticaly untill all is loaded  */
+          setTimeout(function () {
+            doRequestData();
+          }, 100);
+        }
+      } // end regular post
 
     });
 
@@ -274,45 +263,173 @@ jQuery(function ($) {
       setCalendar(calEvents);
     }
 
-
     // hide button if less data then page amount found
     if (result.length < reqvars.ppp && $('.wpajaxbundle.button').length > 0) {
       $('.wpajaxbundle.button').hide();
     }
-
     // trigger after (ie. isotope display)
   }
 
 
-
-  var setCalendar = function(eventlist){
+  var setCalendar = function (eventlist) {
 
     insertHeadScriptTag('https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js');
-    
-    $('#wpajaxbundle').prepend('<div id="calendar"></div>');
+    /* test 3
+    var fullcalcss = $('<style id="fullcalcss" rel="stylesheet" type="text/css" />');
+    let popcss = ".popbox{position:absolute; z-index:999999;display:none; width:300px; min-height:120px; max-height:380px; left:30%; top:38%;overflow:auto; background-color:green; border-radius:8px;}";
+    fullcalcss.append(popcss);
+    $('head').append(fullcalcss);
+*/
+    $('#wpajaxbundle').prepend('<div id="calendar" style="position:relative;"></div>');
+     /*     showendtime: post.custom_field_values["event-hide-end-time"],
+          allDay: post.custom_field_values["event-all-day"],
+          location: post.custom_field_values["event-location"],
+          link: post.custom_field_values["event-link"],
+          linktext: post.custom_field_values["event-link-label"],
+          linktarget: post.custom_field_values["event-link-target"],
+          linktitle: post.custom_field_values["event-link-title"],
+          linkimg: post.custom_field_values["event-link-image"],
+          imgthumbid: post.custom_field_values["_thumbnail_id"],*/
+          
+    /* test 3
+    let pophtml = '<div id="fullCalModal" style="display:none;">'
+    +'<div>ID: <span id="modalID"></span></div>'
+    +'<div>Title: <span id="modalTitle"></span></div>'
+    +'<div>Start Date: <span id="modalStartDate"></span></div>'
+    +'<div>End Date: <span id="modalEndDate"></span></div>'
+    +'</div>';
+    $('#wpajaxbundle').prepend( pophtml );
+    */
+
+
+    //var popstyle = 'position:absolute; z-index:999999;display:none; width:300px; min-height:120px; max-height:380px; left:30%; top:38%;overflow:auto; background-color:green; border-radius:8px;';
+    $('#calendar').prepend('<div class="popbox"><div class="close"><span>×</span></div></div>');
 
     var calendarEl = $('body').find('#calendar');
+
+    $('#calendar .popbox .close').click(function (event) {
+      $('#calendar .popbox').removeClass('active').fadeOut();
+
+    });
+
+    $('#calendar .popbox').on('mouseleave', function (event) {
+
+      $('#calendar .popbox').removeClass('active');
+      setTimeout(function () {
+        if (!$('#calendar .popbox').hasClass('active')) {
+          $('#calendar .popbox').fadeOut();
+        }
+      }, 900);
+
+    });
+
+    $('#calendar .popbox').on('mouseenter', function (event) {
+      if (!$('#calendar .popbox').hasClass('active')) {
+        $('#calendar .popbox').addClass('active').show();
+      }
+    });
+
+
+    /* test cursor follow / reposition box on outside view
+    $(document).on('mousemove', function(e){
+      if( e.clientX > ( $(window).width() - $('.popbox').width() )){
+        //$('.floatbox.display').addClass('left');
+        $('.popbox').css('left', (e.clientX - ($('.popbox').width() + 20 ) )+'px'); // epageX
+      }else{
+        $('.popbox').css('left', (e.clientX+20)+'px');
+      }
+      if( e.clientY > ( $(window).height() - $('.popbox').height() )){
+        $('.popbox').css('top', (e.clientY - ( $('.popbox').height() - 20 ) )+'px');
+      }else{
+        $('.popbox').css('top',  (e.clientY+20)+'px');
+      }
+      //$('.floatbox.display').css('top',  (e.clientY+20)+'px'); // e.pageY
+      //$('.floatbox.display').css('left', (e.clientX+20)+'px'); // epageX
+    });
+    */
+
     var calendar = new FullCalendar.Calendar(calendarEl, {
       header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+        left: 'title',
+        right: 'today', // 'month,agendaWeek,agendaDay',
+        center: 'prev,next',
       },
       theme: 'standard',
       timeZone: 'UTC',
-    firstDay: '1',
-    initialView: 'multiMonthYear', //dayGridMonth
-    multiMonthMaxColumns: 2, 
+      firstDay: '1',
+      initialView: 'multiMonthYear', //dayGridMonth
+      multiMonthMaxColumns: 2,
       events: eventlist,
-    }); 
-    calendar.render(); 
+      /* test 3
+      eventClick: function(eventObj, jsEvent, view) {
+        
+        
+        $('#modalID').html(eventObj.id);
+        $('#modalTitle').html(eventObj.title);
+        //$('#modalLocation').html(eventObj.extendedProps.location);
+        $('#modalStartDate').html(eventObj.start);
+        if(eventObj.end != ''){
+          $('#modalEndDate').html(eventObj.end);
+        }
+        $('#fullCalModal').insertBefore(jsEvent.currentTarget).fadeIn(); //changed just for demo purposes
+  
+        return false;
+      },
+      */
+      // test 2
+      eventClick: function (eventObj, jsEvent, view) {
+        if (eventObj.url) {
+
+          console.log(jsEvent);
+          // set popbox
+          //let boxLeft =  jsEvent.clientX+ "px";//jsEvent.clientX - jsEvent.offsetLeft + "px";//(jsEvent.clientY-80)+'px';
+          //let boxTop = jsEvent.clientY+ "px";//jsEvent.clientY - jsEvent.offsetTop + "px";//(jsEvent.clientX-180)+'px';
+          // set content
+          $('#wpajaxbundle .popbox').find('.innerwrap').remove();
+          let img = '';
+          if (eventObj.imgurl) {
+            img = '<img src="' + eventObj.imgurl + '" />';
+          }
+          let eventinfo = $('<div class="innerwrap"><h3>' + eventObj.title + '</h3><div>' + img + '' + eventObj.content + '</div><div>');
+          $('#wpajaxbundle .popbox').append(eventinfo).fadeIn(); //.insertBefore(jsEvent.currentTarget)
+
+
+
+          return false; //no link follow .. window.open(eventObj.url); 
+
+        } else {
+          alert('Clicked ' + eventObj.title);
+        }
+      },
+
+      /* test 1
+      eventClick: function(calEvent, jsEvent, view) {
+        // call your javascript function to open modal or popup here
+        alert('Event: ' + calEvent.title);
+        alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+        alert('View: ' + view.name);
+        
+        // change the border color just for fun
+        $(this).css('border-color', 'red');
+        
+         },
+      */
+      /*
+          fetch(eventObj.url)
+            .then(function (response) {
+                response.text().then(function (responseText) {
+                    console.log(responseText);
+                    alert('Clicked ' + eventObj.title);
+                });
+            });
+      */
+    });
+    calendar.render();
     //var event = { id: 1, title: 'New event', start: new Date() };
     //calendar.addEvent( event );
+  }
 
- 
-  } 
-
-  var insertHeadScriptTag = function(scripturl){
+  var insertHeadScriptTag = function (scripturl) {
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = scripturl;
@@ -320,12 +437,10 @@ jQuery(function ($) {
     console.log('Fullcalendar script loaded');
   }
 
-  var stampToGMTdateString = function(timestamp){
+  var stampToGMTdateString = function (timestamp) {
     let vardate = new Date(timestamp * 1000);
     return vardate.toGMTString();
   }
-
-
 
   $('body').on('click', '.wpajaxbundle.button', function () {
     doRequestData();
@@ -333,17 +448,13 @@ jQuery(function ($) {
 
   // onscroll load more
   $(document).on('scroll', function () {
-
     if ($('#wpajaxbundle').length > 0 && pullend != true) {
-
       var container = $('#wpajaxbundle');
       var scrollHeight = $(document).height();
       var scrollPosition = $(window).height() + $(window).scrollTop();
-
       //if ((scrollHeight - scrollPosition) / scrollHeight <= 0.01 ) { // for full page end
       if (scrollPosition > (container.offset().top + container.height())) { // on container end
         if (!pullend) {
-
           if ($('.wpajaxbundle.loader').length > 0) {
             $('.wpajaxbundle.loader').fadeIn();
           }
@@ -356,7 +467,6 @@ jQuery(function ($) {
   });
 
   $(document).ready(function () {
-
     doRequestData();
   });
 
--- wp-ajax-bundle.php
+++ wp-ajax-bundle.php
@@ -29,11 +29,14 @@ class WPAjaxBundle
     // Enqueue the wp ajax php scripts
     add_action('wp_enqueue_scripts', array($this, 'getPostData_localize_ajax'));
     add_action('wp_enqueue_scripts', array($this, 'getPostData_ajax_script'));
+    add_action( 'wp_enqueue_scripts', array($this, 'wpajaxbundle_theme_css'));
+
     // Enqueue the wp ajax script on the back end (wp-admin)
-    add_action('admin_enqueue_scripts', array($this, 'getPostData_ajax_script'));
+    add_action('admin_enqueue_scripts', array($this, 'getPostData_ajax_script')); 
     // assign php function for ajax request (bind the nonce)
     add_action('wp_ajax_getWPPostData', array($this, 'getWPPostData'));
     add_action('wp_ajax_nopriv_getWPPostData', array($this, 'getWPPostData'));
+
   }
 
   public function getPostData_localize_ajax()
@@ -47,11 +50,16 @@ class WPAjaxBundle
 
   public function getPostData_ajax_script()
   {
+
+    //wp_enqueue_style( 'wpajaxbundle-css', plugin_dir_path(__FILE__) . '/css/wpajaxbundle.css' ); 
+
     // secure with local script file assigned
     wp_enqueue_script('ajax-script', plugins_url('js/post_ajax.js', __FILE__), array('jquery'), null, true);
     wp_localize_script('ajax-script', 'ajax_data', array(
       'ajaxurl' => admin_url('admin-ajax.php'),
     ));
+
+   
 
     //https://github.com/fullcalendar/fullcalendar
     // https://fullcalendar.io/docs/upgrading-from-v5
@@ -59,10 +67,13 @@ class WPAjaxBundle
     //https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js 
     //wp_enqueue_style('fullcalendar-min-css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.css');
     //wp_enqueue_style('fullcalendar-print-css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.print.css');
-
     //wp_enqueue_script('fullcalendar-script', 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js', __FILE__, array('jquery'), null, true);
  
   }
+
+  public function wpajaxbundle_theme_css() {
+    wp_enqueue_style( 'wpajaxbundle-css', plugins_url('css/wpajaxbundle.css', __FILE__), array(), '0.1', );
+  } 
 
   public function getWPPostData()
   {
